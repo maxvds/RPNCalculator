5a5681d31017b4f98fc18a45bb05e678b27437c4
harrypirrit <harrypirrit@icloud.com>
2021-04-30 17:05:42 +1200

    changed Stack type to Integer

--- RPNApp.java
+++ RPNApp.java
@@ -1,11 +1,13 @@
-package assignment;
+package RCPAssignment;
 //import java.util.Stack;
 
 public class RPNApp{
 
 public static void main(String[] args) {
 	String input = "3 4 * 5 2 + -";
-	Stack<String> stack = new ArrayStack<String>();
+	Stack<Integer> stack = new ArrayStack<Integer>();
+
+
 
 	
 }
